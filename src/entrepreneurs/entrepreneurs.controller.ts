4217e69355ca277b223ca455b2de5219ed90c9e7
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2024-08-28 20:25:39 -0300

    feat: Add missing newline at end of file in schema.prisma

--- src/entrepreneurs/entrepreneurs.controller.ts
+++ src/entrepreneurs/entrepreneurs.controller.ts
@@ -2,32 +2,44 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { EntrepreneursService } from './entrepreneurs.service';
 import { CreateEntrepreneurDto } from './dto/create-entrepreneur.dto';
 import { UpdateEntrepreneurDto } from './dto/update-entrepreneur.dto';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('entrepreneurs')
 @Controller('entrepreneurs')
 export class EntrepreneursController {
-  constructor(private readonly entrepreneursService: EntrepreneursService) {}
+  constructor(private readonly entrepreneursService: EntrepreneursService) { }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new entrepreneur' })
+  @ApiResponse({ status: 201, description: 'The entrepreneur has been successfully created.' })
   create(@Body() createEntrepreneurDto: CreateEntrepreneurDto) {
     return this.entrepreneursService.create(createEntrepreneurDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all entrepreneurs' })
+  @ApiResponse({ status: 200, description: 'The entrepreneurs have been successfully retrieved.' })
   findAll() {
     return this.entrepreneursService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get an entrepreneur by id' })
+  @ApiResponse({ status: 200, description: 'The entrepreneur has been successfully retrieved.' })
   findOne(@Param('id') id: string) {
     return this.entrepreneursService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update an entrepreneur by id' })
+  @ApiResponse({ status: 200, description: 'The entrepreneur has been successfully updated.' })
   update(@Param('id') id: string, @Body() updateEntrepreneurDto: UpdateEntrepreneurDto) {
     return this.entrepreneursService.update(+id, updateEntrepreneurDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete an entrepreneur by id' })
+  @ApiResponse({ status: 200, description: 'The entrepreneur has been successfully deleted.' })
   remove(@Param('id') id: string) {
     return this.entrepreneursService.remove(+id);
   }
