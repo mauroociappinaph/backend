4de63cfc831bfcff4acb94418bd17bbd80c63e7a
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2024-09-10 11:29:49 -0300

    feat: Agregar anotaciones Swagger al controlador de autenticación
    Se han agregado anotaciones Swagger al controlador de autenticación para documentar los endpoints de inicio de sesión y ruta protegida. Esto permitirá generar una documentación clara y precisa de la API. Además, se ha añadido una categoría "auth" para los endpoints relacionados con la autenticación.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { JwtAuthGuard } from './jwt.strategy/jwt-auth.guard'; // Este guard protegerá las rutas
+import { JwtAuthGuard } from './jwt.strategy/jwt-auth.guard';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('auth')  // Define una categoría "auth" para los endpoints
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
     @Post('login')
+    @ApiOperation({ summary: 'Iniciar sesión' })
+    @ApiResponse({ status: 201, description: 'Usuario autenticado con éxito.' })
+    @ApiResponse({ status: 401, description: 'Credenciales incorrectas.' })
+    @ApiBody({
+        description: 'Datos del usuario para autenticación',
+        schema: {
+            example: {
+                username: 'usuarioEjemplo',
+                password: 'contraseña123'
+            }
+        }
+    })
     async login(@Request() req) {
         // Lógica de autenticación del login
         return this.authService.login(req.body);
@@ -14,6 +28,10 @@ export class AuthController {
 
     @UseGuards(JwtAuthGuard)
     @Post('protected')
+    @ApiOperation({ summary: 'Ruta protegida' })
+    @ApiBearerAuth()  // Indica que este endpoint necesita autenticación Bearer
+    @ApiResponse({ status: 200, description: 'Acceso autorizado.' })
+    @ApiResponse({ status: 401, description: 'No autorizado.' })
     getProtected(@Request() req) {
         return req.user; // Información del usuario autenticado
     }
